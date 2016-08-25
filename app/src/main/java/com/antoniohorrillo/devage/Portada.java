5f2d29cc978e6961aa98bbd7b06840628bd45253
Antonio Horrillo Horrillo <antoniohh76@gmail.com>
2016-08-26 01:01:26 +0200

    DevAge
    DevAge en desarrollo.

--- app/src/main/java/com/antoniohorrillo/devage/Portada.java
+++ app/src/main/java/com/antoniohorrillo/devage/Portada.java
@@ -1,7 +1,10 @@
 package com.antoniohorrillo.devage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Portada extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Portada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
+        Button btnEntrar = (Button)findViewById(R.id.btn_entrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                entrar();
+            }
+        });
+    }
+
+    public void entrar() {
+        if (SaveSharedPreference.getUserName(getApplicationContext()).length() == 0) {
+            Intent intent = new Intent(getApplicationContext(), Principal.class);
+            startActivity(intent);
+            this.finish();
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), Principal.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 }
